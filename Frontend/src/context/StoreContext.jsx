0d3d37357d4cc28cdc006867856fc8ce40e90b38
Partha Maity <162608800+ParthaMai@users.noreply.github.com>
2025-07-29 01:21:10 +0530

    Add deployment url

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const[cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://tomato-1d03.onrender.com";
     const[token,setToken] = useState("");
     // this is for fetch ta food list from mongodb
     const[food_list,setFoodList] = useState([]);
